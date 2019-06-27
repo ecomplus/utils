369c5afe6b483a5d44a2c2ed908d97b081548b30
Leonardo Matos <leomp120894@gmail.com>
2019-06-27 12:54:52 -0300

    build: fix handling node env and output filename on webpack base config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const devMode = process.env.NODE_ENV === 'production'
+const devMode = process.env.NODE_ENV !== 'production'
 const path = require('path')
 
 module.exports = {
   mode: devMode ? 'development' : 'production',
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     library: 'ecomUtils',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'ecom-utils.js'
+    filename: 'ecom-utils.min.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'test'),
