8bbbb2d105275a5e50e0b991ecf40f0f94c21d4d
Leonardo Matos <leomp120894@gmail.com>
2019-06-27 16:34:13 -0300

    docs: setup generated docs with jsdoc

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ import currency from './methods/currency'
 
 const _self = name + '@' + version
 
+/**
+ * Exports {@link ecomUtils} object.
+ * @module @ecomplus/utils
+ * @returns {Object} {@link ecomUtils}
+ * @example import '@ecomplus/utils'
+ * @example require('@ecomplus/utils')
+ * @example <script src="https://cdn.jsdelivr.net/npm/@ecomplus/utils"></script>
+ */
+
 export {
   _self,
   i18n,
