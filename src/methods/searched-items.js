4e51a5d81d7c08c24ecb939f62ebeb0b55b5c9d4
Leonardo Matos <leomp120894@gmail.com>
2019-07-02 13:25:56 -0300

    docs(searched-items): examples for 'searchedItems' method

--- src/methods/searched-items.js
+++ src/methods/searched-items.js
@@ -8,6 +8,9 @@ const searchedItems = result => {
       hits = result.hits.hits || result.hits
     }
   }
+
+  // setup items list
+  // array of nested objects
   let items = []
   if (Array.isArray(hits)) {
     // map items array from ELS hits list
@@ -27,7 +30,16 @@ const searchedItems = result => {
  * @returns {array}
  *
  * @example
- * // TODO
+ * // Full Search API response samples:
+ * // https://developers.e-com.plus/docs/api/#/search/items/items
+ * const result = { took: 6, hits: { total: 2, hits: [] } }
+ * result.hits.hits.push({ _id: '123', _source: { sku: 'TEST', name: 'Test' } })
+ * result.hits.hits.push({ _id: '456', _source: { sku: 'SMP', name: 'Smp' } })
+ * ecomUtils.searchedItems(result)
+ * // => [ { _id: '123', sku: 'TEST', name: 'Test' }, { _id: '456', sku: 'SMP', name: 'Smp' } ]
+ * // Same passing the `hits` array as param
+ * ecomUtils.searchedItems(result.hits.hits)
+ * // => [ { _id: '123', sku: 'TEST', name: 'Test' }, { _id: '456', sku: 'SMP', name: 'Smp' } ]
  *
  * @example
  * // Importing this method standalone
