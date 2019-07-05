337d1f236a0efa07fe777985185d97aaeffd151a
Leonardo Matos <leomp120894@gmail.com>
2019-07-05 12:53:57 -0300

    fix(img): trying 'picture' prop to handle cart item object

--- src/methods/img.js
+++ src/methods/img.js
@@ -1,38 +1,47 @@
 const img = (product, pictureId, size = 'normal') => {
-  let pictures
-  if (Array.isArray(product)) {
-    // received list of pictures ?
-    pictures = product
-  } else if (product) {
-    pictures = product.pictures
-  }
-  let picture = Array.isArray(pictures)
-    // select one img object from product pictures
-    ? (pictureId && pictures.filter(pic => pic._id === pictureId)[0]) || pictures[0]
-    // received picture as param ?
-    : product
-
-  let img
-  if (typeof picture === 'object' && picture !== null) {
-    img = picture[size]
-    if (!img) {
-      // try with any size
-      let sizes = [ 'big', 'normal', 'zoom', 'small' ]
-      for (let i = 0; i < sizes.length; i++) {
-        let size = sizes[i]
-        if (picture[size]) {
-          return picture[size]
-        }
-      }
-      // last try with custom sizes
-      for (let size in picture) {
-        if (picture[size] && picture[size].url) {
-          return picture[size]
+  if (product) {
+    // product object has 'pictures'
+    // cart item object has 'picture'
+    let { pictures, picture } = product
+    if (!picture) {
+      if (!pictures) {
+        if (Array.isArray(product)) {
+          // received list of pictures ?
+          pictures = product
+        } else {
+          // received picture object ?
+          picture = product
         }
       }
     }
+    if (Array.isArray(pictures)) {
+      // select one img object from product pictures
+      picture = (pictureId && pictures.filter(pic => pic._id === pictureId)[0]) || pictures[0]
+    }
+
+    let img
+    if (typeof picture === 'object' && picture !== null) {
+      img = picture[size]
+      if (!img) {
+        // try with any size
+        let sizes = [ 'big', 'normal', 'zoom', 'small' ]
+        for (let i = 0; i < sizes.length; i++) {
+          let size = sizes[i]
+          if (picture[size]) {
+            return picture[size]
+          }
+        }
+        // last try with custom sizes
+        for (let size in picture) {
+          if (picture[size] && picture[size].url) {
+            return picture[size]
+          }
+        }
+      }
+    }
+    return img
   }
-  return img
+  return undefined
 }
 
 /**
