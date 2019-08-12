204f9a5b55134f7354423b4c15ab1aa98110c137
Leonardo Matos <leomp120894@gmail.com>
2019-08-12 19:46:58 -0300

    fix(export): exporting both named methods and default with all

---
+++ src/ecom-utils.js
@@ -0,0 +1,70 @@
+/**
+ * @namespace ecomUtils
+ */
+
+import _self from './lib/self'
+import _config from './lib/config'
+
+import i18n from './methods/i18n'
+import name from './methods/name'
+import alphabeticalSort from './methods/alphabetical-sort'
+import price from './methods/price'
+import onPromotion from './methods/on-promotion'
+import formatMoney from './methods/format-money'
+import formatDate from './methods/format-date'
+import findByProperty from './methods/find-by-property'
+import findBySlug from './methods/find-by-slug'
+import findByName from './methods/find-by-name'
+import filterByParentSlug from './methods/filter-by-parent-slug'
+import minQuantity from './methods/min-quantity'
+import inStock from './methods/in-stock'
+import searchedItems from './methods/searched-items'
+import recommendedIds from './methods/recommended-ids'
+import categoriesList from './methods/categories-list'
+import specValues from './methods/spec-values'
+import specTextValue from './methods/spec-text-value'
+import specValueByText from './methods/spec-value-by-text'
+import variationsGrids from './methods/variations-grids'
+import gridTitle from './methods/grid-title'
+import img from './methods/img'
+import imgSizes from './methods/img-sizes'
+import nickname from './methods/nickname'
+import phone from './methods/phone'
+import birthDate from './methods/birth-date'
+import lineAddress from './methods/line-address'
+import objectIdPad from './methods/object-id-pad'
+import randomObjectId from './methods/random-object-id'
+
+export {
+  _self,
+  _config,
+  i18n,
+  name,
+  alphabeticalSort,
+  price,
+  onPromotion,
+  formatMoney,
+  formatDate,
+  findByProperty,
+  findBySlug,
+  findByName,
+  filterByParentSlug,
+  minQuantity,
+  inStock,
+  searchedItems,
+  recommendedIds,
+  categoriesList,
+  specValues,
+  specTextValue,
+  specValueByText,
+  variationsGrids,
+  gridTitle,
+  img,
+  imgSizes,
+  nickname,
+  phone,
+  birthDate,
+  lineAddress,
+  objectIdPad,
+  randomObjectId
+}
--- src/index.js
+++ src/index.js
@@ -2,42 +2,8 @@
  * https://github.com/ecomclub/ecomplus-utils
  * @author E-Com Club <ti@e-com.club>
  * @license MIT
- */
-
-import _self from './lib/self'
-import _config from './lib/config'
-
-import i18n from './methods/i18n'
-import name from './methods/name'
-import alphabeticalSort from './methods/alphabetical-sort'
-import price from './methods/price'
-import onPromotion from './methods/on-promotion'
-import formatMoney from './methods/format-money'
-import formatDate from './methods/format-date'
-import findByProperty from './methods/find-by-property'
-import findBySlug from './methods/find-by-slug'
-import findByName from './methods/find-by-name'
-import filterByParentSlug from './methods/filter-by-parent-slug'
-import minQuantity from './methods/min-quantity'
-import inStock from './methods/in-stock'
-import searchedItems from './methods/searched-items'
-import recommendedIds from './methods/recommended-ids'
-import categoriesList from './methods/categories-list'
-import specValues from './methods/spec-values'
-import specTextValue from './methods/spec-text-value'
-import specValueByText from './methods/spec-value-by-text'
-import variationsGrids from './methods/variations-grids'
-import gridTitle from './methods/grid-title'
-import img from './methods/img'
-import imgSizes from './methods/img-sizes'
-import nickname from './methods/nickname'
-import phone from './methods/phone'
-import birthDate from './methods/birth-date'
-import lineAddress from './methods/line-address'
-import objectIdPad from './methods/object-id-pad'
-import randomObjectId from './methods/random-object-id'
-
-/**
+ *
+ * @description
  * JS utility functions for E-Com Plus (not only) related apps.
  * @module @ecomplus/utils
  * @see ecomUtils
@@ -55,40 +21,9 @@ import randomObjectId from './methods/random-object-id'
  * <script src="https://cdn.jsdelivr.net/npm/@ecomplus/utils/dist/ecom-utils.polyfill.min.js"></script>
  */
 
-export {
-  _self,
-  _config,
-  i18n,
-  name,
-  alphabeticalSort,
-  price,
-  onPromotion,
-  formatMoney,
-  formatDate,
-  findByProperty,
-  findBySlug,
-  findByName,
-  filterByParentSlug,
-  minQuantity,
-  inStock,
-  searchedItems,
-  recommendedIds,
-  categoriesList,
-  specValues,
-  specTextValue,
-  specValueByText,
-  variationsGrids,
-  gridTitle,
-  img,
-  imgSizes,
-  nickname,
-  phone,
-  birthDate,
-  lineAddress,
-  objectIdPad,
-  randomObjectId
-}
+import * as ecomUtils from './ecom-utils'
 
-/**
- * @namespace ecomUtils
- */
+export default ecomUtils
+
+// named exports off all methods
+export * from './ecom-utils'
