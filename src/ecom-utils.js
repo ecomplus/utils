de89548b62aec283dcbf9a4cb34c0c75b1903862
Leonardo Matos <leomp120894@gmail.com>
2019-11-22 19:49:12 -0300

    feat(parse-date): add new 'parseDate' method

--- src/ecom-utils.js
+++ src/ecom-utils.js
@@ -31,8 +31,9 @@ import img from './methods/img'
 import imgSizes from './methods/img-sizes'
 import nickname from './methods/nickname'
 import phone from './methods/phone'
-import parsePhone from './methods/parse-phone'
 import birthDate from './methods/birth-date'
+import parsePhone from './methods/parse-phone'
+import parseDate from './methods/parse-date'
 import lineAddress from './methods/line-address'
 import objectIdPad from './methods/object-id-pad'
 import randomObjectId from './methods/random-object-id'
@@ -78,8 +79,9 @@ export {
   imgSizes,
   nickname,
   phone,
-  parsePhone,
   birthDate,
+  parsePhone,
+  parseDate,
   lineAddress,
   objectIdPad,
   randomObjectId
---
+++ src/methods/parse-date.js
@@ -0,0 +1,47 @@
+import _config from './../lib/config'
+
+const parsePhone = (dateStr, countryCode = _config.get('country_code')) => {
+  // parse country formatted date string to { day, month, year }
+  let day, month, year
+  if (typeof dateStr === 'string') {
+    const dateNumber = (start, ln) => parseInt(dateStr.substr(start, ln), 10)
+    // fix date string to digits only first
+    dateStr = dateStr.replace(/[\D]/g, '')
+    if (countryCode === 'BR') {
+      day = dateNumber(0, 2)
+      month = dateNumber(2, 2)
+      year = dateNumber(4, 4)
+    } else {
+      day = dateNumber(6, 2)
+      month = dateNumber(4, 2)
+      year = dateNumber(0, 4)
+    }
+  }
+  return { day, month, year }
+}
+
+/**
+ * @method
+ * @memberof ecomUtils
+ * @name parseDate
+ * @description Returns customer birth date object from string.
+ * @param {string} dateStr - Formatted date string
+ * @param {string} [countryCode=_config.get('country_code')] - Country ISO 3166-1 alpha-2 code
+ * @returns {object}
+ *
+ * @example
+ * // Date string is fixed to digits only and parsed to E-Com Plus APIs date object
+ * ecomUtils.parseDate('1990-01-12')
+ * // => { day: 12, month: 1, year: 1990 }
+ * ecomUtils.parseDate('10/02/1997', 'BR')
+ * // => { day: 10, month: 2, year: 1997 }
+ *
+ * @example
+ * // You can also set the configured country code first
+ * ecomUtils._config.set('country_code', 'BR')
+ * // Then call `parseDate` without expliciting country code again
+ * ecomUtils.parseDate('10/02/1997')
+ * // => { day: 10, month: 2, year: 1997 }
+ */
+
+export default parsePhone
