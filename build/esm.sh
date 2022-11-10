e23e09e1f7bc021380d0a652905b27213853f5c5
Leonardo Matos <leomp120894@gmail.com>
2022-11-09 23:55:47 -0300

    build: fix esm build import with line break [#242]
    fixes "Cannot find module **/constants from **/config.mjs"

--- build/esm.sh
+++ build/esm.sh
@@ -2,6 +2,7 @@
 
 cp -r ./src ./dist/esm
 find dist/esm -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/([ie][mx]port [^']+?)'(\\.[^']+)'/\\1'\\2.mjs'/g"
+find dist/esm -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/(} from )'(\\.[^']+)'/\\1'\\2.mjs'/g"
 find dist/esm -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/.([cm])js.mjs/.\\1js/g"
 for file in ./dist/esm/*.js; do
   mv -- "$file" "${file%.js}.mjs"
