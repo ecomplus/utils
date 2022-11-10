dde17b8ee36871668c2e105c701e0c577038b0ca
Leonardo Matos <leomp120894@gmail.com>
2022-11-09 23:16:14 -0300

    build: build esm to dist with fixed imports and mjs extension [#242]

---
+++ build/esm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r ./src ./dist/esm
+find dist/esm -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/([ie][mx]port [^']+?)'(\\.[^']+)'/\\1'\\2.mjs'/g"
+find dist/esm -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/.([cm])js.mjs/.\\1js/g"
+for file in ./dist/esm/*.js; do
+  mv -- "$file" "${file%.js}.mjs"
+done
+for file in ./dist/esm/**/*.js; do
+  mv -- "$file" "${file%.js}.mjs"
+done
