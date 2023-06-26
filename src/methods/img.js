f46c30be21255fe832b17eb2f409ed336f5686ed
Leonardo Matos <leomp120894@gmail.com>
2023-06-26 19:18:15 -0300

    fix(img): always check `.url` prop
    fix return object type definition to picture size object

--- src/methods/img.js
+++ src/methods/img.js
@@ -9,7 +9,7 @@ import _config from './../lib/config'
  * @param {string} [pictureId] - ObjectID of preferred picture to find in the list
  * @param {string} [size=$ecomConfig.get('default_img_size')] - Preferred image size
  * (generally `normal`) to find on picture object
- * @returns {Object.<string, *>|undefined}
+ * @returns {{ url: string; size?: string; alt?: string; }|undefined}
  *
  * @example
  * const product = { 'pictures': [ { 'small': { 'url': 'https://ecom.com/imgs/100px/64gb.jpg'}, 'big': { 'url': 'https://ecom.com/imgs/700px/64gb.jpg' }, '_id': '694890155127368133600000' }, { 'small': { 'url': 'https://ecom.com/imgs/100px/e-5-64gb.jpg' }, 'big': { 'url': 'https://ecom.com/imgs/700px/e-5-64gb.jpg' }, '_id': '694890155127368133600001' }, { 'small': { 'url': 'https://ecom.com/imgs/100px/5-64gb.jpg' }, 'big': { 'url': 'https://ecom.com/imgs/700px/5-64gb.jpg' }, '_id': '694890155127368133600002' } ], 'name': 'Smartphone Xiaomi' }
@@ -45,40 +45,40 @@ const img = (product, pictureId, size) => {
     let img
     if (typeof picture === 'object' && picture !== null) {
       img = picture[size]
-      if (!img) {
-        // try predefined any size
-        let sizes
-        switch (size) {
-          case 'small':
-            sizes = ['normal', 'big']
-            break
-          case 'normal':
-            sizes = ['big', 'zoom', 'small']
-            break
-          case 'big':
-            sizes = ['zoom', 'normal']
-            break
-          case 'zoom':
-            sizes = ['big']
-            break
-          default:
-            sizes = ['big', 'zoom', 'normal', 'small']
+      if (img && img.url) {
+        return img
+      }
+      // try predefined any size
+      let sizes
+      switch (size) {
+        case 'small':
+          sizes = ['normal', 'big']
+          break
+        case 'normal':
+          sizes = ['big', 'zoom', 'small']
+          break
+        case 'big':
+          sizes = ['zoom', 'normal']
+          break
+        case 'zoom':
+          sizes = ['big']
+          break
+        default:
+          sizes = ['big', 'zoom', 'normal', 'small']
+      }
+      for (let i = 0; i < sizes.length; i++) {
+        const size = sizes[i]
+        if (picture[size] && picture[size].url) {
+          return picture[size]
         }
-        for (let i = 0; i < sizes.length; i++) {
-          const size = sizes[i]
-          if (picture[size]) {
-            return picture[size]
-          }
-        }
-        // last try with custom sizes
-        for (const size in picture) {
-          if (picture[size] && picture[size].url) {
-            return picture[size]
-          }
+      }
+      // last try with custom sizes
+      for (const size in picture) {
+        if (picture[size] && picture[size].url) {
+          return picture[size]
         }
       }
     }
-    return img
   }
   return undefined
 }
